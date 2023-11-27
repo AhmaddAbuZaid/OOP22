c9674d9352553437bc2267196ffef18d4ce5d92a
AhmaddAbuZaid <150189482+AhmaddAbuZaid@users.noreply.github.com>
2023-11-27 12:30:48 +0300

    Add files via upload

---
+++ facadeExample.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+/**
+ *
+ * @author lab1b01
+ */
+public class facadeExample {
+    public static void main(String[] args) {
+        facad facade = new facad();
+        facade.operate();
+    }
+}
